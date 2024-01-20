68727a804fe0d53f473b84eb0094ba13f0854499
stariylist <oleksandr.osadchyi@nure.ua>
2024-01-20 13:30:37 +0200

    first commit

---
+++ lib/details.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:osadchiyy_pmp_laba_1/person.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key, required this.person});
+
+  final Person person;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(person.fullName),
+        ),
+        body: Center(
+          child: Text (person.position),
+        )
+    );
+  }
+}
